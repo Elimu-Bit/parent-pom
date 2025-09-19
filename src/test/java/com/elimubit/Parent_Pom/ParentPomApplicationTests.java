bc337d6d75e0c5dc0b92d199e0135927a93ea118
vicky <dogovicky05@gmail.com>
2025-09-19 12:27:27 +0300

    Initial Commit

---
+++ src/test/java/com/elimubit/Parent_Pom/ParentPomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elimubit.Parent_Pom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentPomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
